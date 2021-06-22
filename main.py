2312bfdae5a0423a7206b05be4cd1b202a68a442
Walter Santacruz <wasantac12349@gmail.com>
2021-06-21 19:25:12 -0500

    lex

--- main.py
+++ main.py
@@ -1,9 +1,52 @@
 import ply.lex as lex
 
-tokens = ('IDENTIFICADOR',)
+tokens = ['IDENTIFICADOR','ENTERO','FLOTANTE','CHARACTER','STRING','COMENTARIO','PUNTERO'] 
+reserved = {
+    'if' : 'IF',
+    'then' : 'THEN',
+    'else' : 'ELSE',
+    'while' : 'WHILE',
+    'for': 'FOR',
+    'int': 'INT',
+    'float': 'FLOAT',
+    'long': 'LONG',
+    'char': 'CHAR',
+    'void': 'VOID',
+    'public': 'PUBLIC',
+    'static' :'STATIC',
+    'struct': 'STRUCT',
+    'true': 'TRUE',
+    'false': 'FALSE',
+    'nullptr' : 'NULLPOINTER',
+    'auto' :'AUTO',
+    'bool' : 'BOOL',
+    'new' : 'NEW',
+    'print': 'PRINT',
+
+}
+tokens = tokens + list(reserved.values())
 
 #TOKENS
-t_IDENTIFICADOR= r'\w+'
+t_PUNTERO = r'\*[a-zA-Z_][A-Za-z0-9_]*'
+def t_IDENTIFICADOR(t):
+    r'[a-zA-Z_][A-Za-z0-9_]*'
+    t.type = reserved.get(t.value,'IDENTIFICADOR') 
+    return t
+t_CHARACTER = r'\'[a-zA-z]\''
+t_STRING = r'(\"[a-zA-z0-9\s]*\"|\'[a-zA-z0-9\s]*\')'
+
+def t_FLOTANTE(t):
+    r'\d+\.\d+'
+    t.value = float(t.value)
+    return t
+def t_ENTERO(t):
+    r'[0-9]+'
+    t.value = int(t.value)
+    return t
+
+def t_COMENTARIO(t):
+    r'(//.*|/\*.*\*/)'
+    pass
 
 #IGNORE
 t_ignore = " \t"
@@ -17,7 +60,7 @@ def t_error(t):
 #BUILDER
 lexer = lex.lex()
 
-data = "Test\nHola"
+data = "*hola identificador1 _identificador2 2.5 786 /*comentario*/ true false \"Esto es una prueba\""
 lexer.input(data)
 
 while True:
